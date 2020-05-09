1a7c5ddef68b5b1886d335572bf00bf1efcf595e
Dhruv <dhruvmisra@live.com>
2020-05-09 23:10:55 +0530

    Project init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Sugarizer Dynamic Tutorial</h1>
+    <CodeSection :code="'<some code>'" />
+  </div>
+</template>
+
+<script>
+import CodeSection from './components/CodeSection.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CodeSection
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/CodeSection.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="code">
+    {{ code }}
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    code: String
+  }
+}
+</script>
+
+<style>
+
+</style>
