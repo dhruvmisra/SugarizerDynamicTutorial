d00a9d87a4df56e51e6938579d5e8f36def8c38b
Dhruv <dhruvmisra@live.com>
2020-05-10 23:06:40 +0530

    Added basic JSON and prism features

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
     <h1>Sugarizer Dynamic Tutorial</h1>
-    <CodeSection :code="'<some code>'" />
+    <CodeSection :code="tutorial ? tutorial.steps[1].content[0] : ''" />
   </div>
 </template>
 
 <script>
 import CodeSection from './components/CodeSection.vue'
+import Tutorial from '@/tutorial.json'
 
 export default {
   name: 'App',
+  data: () => ({
+    tutorial: null,
+    code: `
+define(["sugar-web/activity/activity"], function (activity) {
+
+  // Manipulate the DOM only when it is ready.
+  requirejs(['domReady!'], function (doc) {
+
+    // Initialize the activity.
+    activity.setup();
+
+  });
+
+});
+    `
+  }),
   components: {
     CodeSection
+  },
+  mounted() {
+    this.tutorial = Tutorial;
   }
 }
 </script>
--- src/components/CodeSection.vue
+++ src/components/CodeSection.vue
@@ -1,13 +1,26 @@
 <template>
   <section class="code">
-    {{ code }}
+    <pre>
+      <code class="language-markdown">{{ code }}</code>
+    </pre>
   </section>
 </template>
 
 <script>
+import Prism from 'prismjs';
+import 'prismjs/themes/prism-tomorrow.css';
+import 'prismjs/components/prism-markdown';
+
 export default {
+  data: () => ({
+    html: ''
+  }),
   props: {
     code: String
+  },
+  mounted() {
+    this.html = Prism.highlight(this.code, Prism.languages.markdown, 'markdown');
+    console.log(this.html);
   }
 }
 </script>
